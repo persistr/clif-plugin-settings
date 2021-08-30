032202774b64d29ae81e4398d71c9a81a3fbf197
Sasha Jolich <sasha@ctoatlarge.com>
2021-08-30 00:40:03 -0700

    feat: add ability to set config file name
    By default, the config file name is set to ".cli". This can now be changed with a call to .name(). Example: require('@persistr/clif-plugin-settings').name('mysettings.conf')

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const Settings = require('@persistr/settings')
 const os = require('os')
-module.exports = {
+
+let name = '.cli'
+const settings = {
   initialize: (toolbox) => {
     const isBrowser = typeof window !== 'undefined' && ({}).toString.call(window) === '[object Window]'
     if (isBrowser) {
-      toolbox.settings = Settings.localStorage('.persistr.cli')
+      toolbox.settings = Settings.localStorage(`${name}`)
     }
     else {
-      toolbox.settings = Settings.file(`${os.homedir}/.persistr.cli`)
+      toolbox.settings = Settings.file(`${os.homedir}/${name}`)
     }
   }
 }
+
+settings.name = (n) => {
+  name = n
+  return settings
+}
+
+module.exports = settings
